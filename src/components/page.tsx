3936af0e867b49f39040066a3695e236bba7ff6e
NgocNhi123 <ngocnhi@166>
2024-05-18 22:22:45 +0700

    fix invalid links

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -14,7 +14,7 @@ export function AppPage(props: Props) {
   return (
     <>
       <header className="fixed top-0 left-0 w-full text-center px-32 py-10 space-y-2">
-        <div className="text-lg">{isMain ? title : <Link href="/">{title}</Link>}</div>
+        <div className="text-lg">{isMain ? title : <Link href="/nnhilys/">{title}</Link>}</div>
         <div className="text-sm"><i>{description}</i></div>
       </header>
       <main className="flex flex-col items-center gap-4 text-justify">
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,7 +5,7 @@ import { AppPage } from '../components/page'
 export function Blog() {
   return (
     <AppPage title="nnhilys" isMain>
-      <Link href="/about">About me</Link>
+      <Link href="/nnhilys/about">About me</Link>
       <Card
         title="My first blog"
         description="A story about how I create my first blog"
