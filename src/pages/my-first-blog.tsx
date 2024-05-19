10d888ef8d3bb00fd6e2fb5f0dd62ec70c807d03
NgocNhi123 <nnhily16@gmail.com>
2024-05-19 16:31:47 +0700

    improve writing

--- src/pages/my-first-blog.tsx
+++ src/pages/my-first-blog.tsx
@@ -9,37 +9,36 @@ export function MyFirstBlog() {
     >
       <p>
         Almost everyone I asked
-        <i> "How can I to improve my technical skills" </i>
+        <i> "How can I improve my technical skills?" </i>
         gave me the same answer:
       </p>
       <blockquote>
-        <strong>"Build your own blog, you'll learn something along the way!"</strong>
+        <strong>"Build your own blog, you'll learn a ton along the way!"</strong>
       </blockquote>
       <p>
-        So after 2 and a half years working in the field, I decided to create my
-        own blog. Initially, I wanted to create something with fancy animations
-        and colorful designs. Or maybe something that is different from the
-        others. In the end, I realized those things don't made a good blog. It's
-        the content that matters the most. So here we are with a blog that covers
-        just the basics of the basics.
+        after immersing myself in the tech field for 2 and a half years, I embarked
+        on the journey of creating my own blog. Initially, my imagination ran
+        wild with ideas of fancy animations and vibrant designs, or perhaps
+        crafting something utterly unique. However, I soon realized that the
+        essence of a good blog lies in its content, not just its aesthetics. So
+        here we are with a blog that covers just the basics of the basics.
       </p>
       <p>
-        Although I had set up multiple projects before, this and the deployment
-        process are still among of the things that I struggle with the most.
-        Luckily, Vite documents cover almost everything I need.
+        Although I had set up multiple projects before, setting up and deploying
+        the blog are still among the things that I struggle with the most.
+        Fortunately, Vite's comprehensive documentation provided the guidance I
+        needed, simplifying these processes significantly.
       </p>
       <p>
-        Vite provides different presets for us to choose from. To
-        {' '}
         <a
           href="https://vitejs.dev/guide/#scaffolding-your-first-vite-project"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i>create a Vite project using React & TypeScript</i>
+          <i>To kickstart the project with React & TypeScript</i>
           <sup>[1]</sup>
         </a>
-        , all we need is this command:
+        , I used the command:
       </p>
       <code>
         <strong>npm create vite@latest my-app -- --template react-ts</strong>
@@ -59,8 +58,8 @@ export function MyFirstBlog() {
         .
       </p>
       <p>
-        After all that was done, the final thing I needed to do before I can
-        start writing was the routing. For this one, I opted for
+        After all that was done, the final piece of the puzzle before I could
+        dive into writing was the routing. For this task, I opted for
         {' '}
         <a
           href="https://github.com/molefrog/wouter"
@@ -71,45 +70,41 @@ export function MyFirstBlog() {
           <sup>[3]</sup>
         </a>
         , a lightweight library that is straightforward and requires minimal setup.
-        With just a simple
+        A few
         {' '}
         <a
           href="https://github.com/molefrog/wouter?tab=readme-ov-file#route-pathpattern-"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i>Route component</i>
+          <i>Route</i>
           <sup>[4]</sup>
         </a>
-        {' '}
-        to setup the path and a
+        and
         {' '}
         <a
           href="https://github.com/molefrog/wouter?tab=readme-ov-file#link-hrefpath-"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i>Link component</i>
+          <i>Link</i>
           <sup>[5]</sup>
         </a>
         {' '}
-        to navigate to said path, routing was done and I can start to write my
-        first post.
+        components later, my blog was ready for its first post.
       </p>
       <p>
-        You might wonder, or not, what can we expect from this blog? I would say
-        pretty much anything but mostly tech-related stuff. From the very beginning
-        of my journey, I never had much chance to take a look at HTML semantics
-        so that is one thing I'll focus on when implementing this blog: to use the
-        right tag for the right content. Furthermore, since I'm treating this
-        blog as my writing exercise, you probably won't find anything new here.
-        Finally, for everything else, I'll just go with the flow and see where the
-        path takes us. That's all from me, see you in my next post.
+        What can readers expect from this blog? A mix of everything, with a focus
+        on tech. I'll delve into topics I've overlooked, like HTML semantics,
+        ensuring content is accessible and correctly structured. Since I view
+        this blog as a writing exercise, you probably won't find anything here
+        that you couldn't find elsewhere. Finally, I'm eager to see where
+        this journey takes us. That's all from me, see you in my next post.
       </p>
       <p>
-        Though this blog looks quite boring right now, there's always a not too
-        small probability that I'll get sick of this black and white theme and
-        literally paint the town red.
+        Though this blog looks quite vanilla right now, don't be too surprised
+        if one day I decide to dazzle it up. Who knows? A splash of color might
+        be just around the corner.
       </p>
     </AppPage>
   )
